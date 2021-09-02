b0c8cc17b04d8d0e99c758ad24232acd9937f383
luiz.almeida <luiz.almeida@trixti.com.br>
2021-09-02 15:02:39 -0300

    desafio ignite1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,19 +5,37 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+
+    const newTask = { 
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done:false
+  }
+   setTasks(oldTasks => [...tasks, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+   const updatedTask = tasks.map(task => ({...task})) //principio de imutabilidade
+
+   const foundItem = updatedTask.find(item => item.id === id)
+
+   if(!foundItem)
+    return
+
+    foundItem.done = ! foundItem.done;
+    setTasks(updatedTask)
+    
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const updatedTask = tasks.filter(tasks => tasks.id !== id)
+    setTasks(updatedTask)
   }
 
   return (
